af9e39dc933f05d542631c8d8e7c056eb0c21d13
Gabriel Constantine B. Belandres <96006718+Gabelandres@users.noreply.github.com>
2023-11-24 20:33:28 +0800

    update1

---
+++ lib/core/utils/image_constant.dart
@@ -0,0 +1,27 @@
+class ImageConstant {
+  // Image folder path
+  static String imagePath = 'assets/images';
+
+  // Home images
+  static String imgMenuIcon = '$imagePath/img_menu_icon.png';
+
+  static String imgHomeIcon = '$imagePath/img_home_icon.png';
+
+  static String imgImage12 = '$imagePath/img_image_12.png';
+
+  static String imgImage3 = '$imagePath/img_image_3.png';
+
+  static String imgImage13 = '$imagePath/img_image_13.png';
+
+  static String imgImage14 = '$imagePath/img_image_14.png';
+
+  static String imgImage15 = '$imagePath/img_image_15.png';
+
+  static String imgImage16 = '$imagePath/img_image_16.png';
+
+  static String imgImage17 = '$imagePath/img_image_17.png';
+
+  static String imgImage18 = '$imagePath/img_image_18.png';
+
+  static String imageNotFound = 'assets/images/image_not_found.png';
+}
