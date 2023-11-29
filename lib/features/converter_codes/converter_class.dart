62bd1742410940ca882e5a22fa99f95cfb6aff72
LETrabado <luisemil.trabado@wvsu.edu.ph>
2023-11-29 17:29:19 +0800

    Converter Buttons working

---
+++ lib/features/converter_codes/converter_class.dart
@@ -0,0 +1,9 @@
+import 'package:converter/converter.dart';
+
+class ConverterApp {
+  late String input_value, output_value;
+
+  int areafunc(num input, String in_unit, out_unit) {
+    return 12;
+  }
+}
--- lib/features/distance_function/distance_function.dart
+++ lib/features/distance_function/distance_function.dart
@@ -3,6 +3,10 @@ import 'package:converter_app/core/app_export.dart';
 import 'package:flutter/material.dart';
 
 class CalculatorKeypad extends StatelessWidget {
+  final Function(String) passInput;
+
+  CalculatorKeypad({required this.passInput});
+
   @override
   Widget build(BuildContext context) {
     mediaQueryData = MediaQuery.of(context);
@@ -17,34 +21,114 @@ class CalculatorKeypad extends StatelessWidget {
                 children: [
                   Row(
                     children: [
-                      CalculatorButton("7"),
-                      CalculatorButton("8"),
-                      CalculatorButton("9"),
-                      CalculatorIconButton(Icons.backspace),
+                      CalculatorButton(
+                        "7",
+                        onPressed: () {
+                          passInput("8");
+                        },
+                      ),
+                      CalculatorButton(
+                        "8",
+                        onPressed: () {
+                          passInput("8");
+                        },
+                      ),
+                      CalculatorButton(
+                        "9",
+                        onPressed: () {
+                          passInput("9");
+                        },
+                      ),
+                      CalculatorIconButton(
+                        Icons.backspace,
+                        onPressed: () {
+                          passInput("B");
+                        },
+                      ),
                     ],
                   ),
                   Row(
                     children: [
-                      CalculatorButton("4"),
-                      CalculatorButton("5"),
-                      CalculatorButton("6"),
-                      CalculatorIconButton(Icons.undo),
+                      CalculatorButton(
+                        "4",
+                        onPressed: () {
+                          passInput("4");
+                        },
+                      ),
+                      CalculatorButton(
+                        "5",
+                        onPressed: () {
+                          passInput("5");
+                        },
+                      ),
+                      CalculatorButton(
+                        "6",
+                        onPressed: () {
+                          passInput("6");
+                        },
+                      ),
+                      CalculatorIconButton(
+                        Icons.undo,
+                        onPressed: () {
+                          passInput("U");
+                        },
+                      ),
                     ],
                   ),
                   Row(
                     children: [
-                      CalculatorButton("1"),
-                      CalculatorButton("2"),
-                      CalculatorButton("3"),
-                      CalculatorIconButton(Icons.swap_horiz),
+                      CalculatorButton(
+                        "1",
+                        onPressed: () {
+                          passInput("1");
+                        },
+                      ),
+                      CalculatorButton(
+                        "2",
+                        onPressed: () {
+                          passInput("2");
+                        },
+                      ),
+                      CalculatorButton(
+                        "3",
+                        onPressed: () {
+                          passInput("3");
+                        },
+                      ),
+                      CalculatorIconButton(
+                        Icons.swap_horiz,
+                        onPressed: () {
+                          passInput("S");
+                        },
+                      ),
                     ],
                   ),
                   Row(
                     children: [
-                      CalculatorButton("."),
-                      CalculatorButton("0"),
-                      CalculatorButton("C"),
-                      CalculatorButton("="),
+                      CalculatorButton(
+                        ".",
+                        onPressed: () {
+                          passInput(".");
+                        },
+                      ),
+                      CalculatorButton(
+                        "0",
+                        onPressed: () {
+                          passInput("0");
+                        },
+                      ),
+                      CalculatorButton(
+                        "C",
+                        onPressed: () {
+                          passInput("C");
+                        },
+                      ),
+                      CalculatorButton(
+                        "=",
+                        onPressed: () {
+                          passInput("=");
+                        },
+                      ),
                     ],
                   ),
                 ],
@@ -62,9 +146,13 @@ class CalculatorButton extends StatelessWidget {
   final Color? color;
   final double fontSize;
   final double height;
+  final VoidCallback onPressed;
 
   CalculatorButton(this.text,
-      {this.color, this.fontSize = 25, this.height = 50,});
+      {this.color,
+      this.fontSize = 25,
+      this.height = 50,
+      required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
@@ -73,7 +161,9 @@ class CalculatorButton extends StatelessWidget {
         height: height,
         margin: EdgeInsets.all(4.0),
         child: ElevatedButton(
-          onPressed: () {},
+          onPressed: () {
+            onPressed();
+          },
           style: ElevatedButton.styleFrom(
             backgroundColor: color ?? const Color.fromRGBO(255, 255, 255, 1),
             shape: RoundedRectangleBorder(
@@ -82,9 +172,10 @@ class CalculatorButton extends StatelessWidget {
           ),
           child: Text(
             text,
-            style: TextStyle(fontSize: fontSize, 
-            color: Colors.black,
-            fontFamily: 'Lato',
+            style: TextStyle(
+              fontSize: fontSize,
+              color: Colors.black,
+              fontFamily: 'Lato',
             ),
           ),
         ),
@@ -97,8 +188,10 @@ class CalculatorIconButton extends StatelessWidget {
   final IconData icon;
   final Color? color;
   final double height;
+  final VoidCallback onPressed;
 
-  CalculatorIconButton(this.icon, {this.color, this.height = 50.0});
+  CalculatorIconButton(this.icon,
+      {this.color, this.height = 50.0, required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
@@ -108,7 +201,9 @@ class CalculatorIconButton extends StatelessWidget {
           height: height,
           margin: EdgeInsets.all(4.0),
           child: ElevatedButton(
-            onPressed: () {},
+            onPressed: () {
+              onPressed();
+            },
             style: ElevatedButton.styleFrom(
               backgroundColor: color ?? const Color.fromRGBO(255, 255, 255, 1),
               shape: RoundedRectangleBorder(
@@ -123,7 +218,6 @@ class CalculatorIconButton extends StatelessWidget {
   }
 }
 
-
 class DistanceFunction extends StatefulWidget {
   const DistanceFunction({Key? key}) : super(key: key);
 
@@ -133,6 +227,57 @@ class DistanceFunction extends StatefulWidget {
 
 class _DistanceFunctionState extends State<DistanceFunction> {
   String selectedUnit = "Meter";
+  String input_value = '';
+  void equals() {
+    print('equals');
+  }
+
+  void clear() {
+    print('clear');
+    setState(() {
+      input_value = '';
+    });
+  }
+
+  void backSpace() {
+    print('backspace');
+  }
+
+  void undo() {
+    print('undo');
+  }
+
+  void swap() {
+    print('swap');
+  }
+
+  void addInput(String input) {
+    setState(() {
+      input_value += input;
+    });
+  }
+
+  void getInput(String input) {
+    switch (input) {
+      case "=":
+        equals();
+        break;
+      case "C":
+        clear();
+        break;
+      case "B":
+        backSpace();
+        break;
+      case "U":
+        undo();
+        break;
+      case "S":
+        swap();
+        break;
+      default:
+        addInput(input);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -181,12 +326,13 @@ class _DistanceFunctionState extends State<DistanceFunction> {
                           padding: EdgeInsets.all(30.0),
                           margin: EdgeInsets.all(5.0),
                           decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(13.0),
-                  ),
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(13.0),
+                          ),
                           child: Center(
                             child: Text(
-                              '',
+                              //Input Values===========================================
+                              input_value,
                               style: TextStyle(color: Colors.black),
                             ),
                           ),
@@ -198,12 +344,13 @@ class _DistanceFunctionState extends State<DistanceFunction> {
                           padding: EdgeInsets.all(30.0),
                           margin: EdgeInsets.all(30.0),
                           decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(13.0),
-                  ),
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(13.0),
+                          ),
                           child: Center(
                             child: Text(
-                              '',
+                              //Input Units===========================================
+                              'Test4',
                               style: TextStyle(color: Colors.black),
                             ),
                           ),
@@ -215,7 +362,6 @@ class _DistanceFunctionState extends State<DistanceFunction> {
               ),
             ],
           ),
-
           Row(
             children: [
               Expanded(
@@ -234,12 +380,13 @@ class _DistanceFunctionState extends State<DistanceFunction> {
                           padding: EdgeInsets.all(30.0),
                           margin: EdgeInsets.all(5.0),
                           decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(13.0),
-                            ),
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(13.0),
+                          ),
                           child: Center(
                             child: Text(
-                              '',
+                              //Output Values===========================================
+                              'Test1',
                               style: TextStyle(color: Colors.black),
                             ),
                           ),
@@ -251,12 +398,13 @@ class _DistanceFunctionState extends State<DistanceFunction> {
                           padding: EdgeInsets.all(30.0),
                           margin: EdgeInsets.all(30.0),
                           decoration: BoxDecoration(
-                               color: Colors.white,
-                               borderRadius: BorderRadius.circular(13.0),
-                            ),
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(13.0),
+                          ),
                           child: Center(
                             child: Text(
-                              '',
+                              //Output Units===========================================
+                              'Test2',
                               style: TextStyle(color: Colors.black),
                             ),
                           ),
@@ -268,8 +416,9 @@ class _DistanceFunctionState extends State<DistanceFunction> {
               ),
             ],
           ),
-
-           CalculatorKeypad(),
+          CalculatorKeypad(
+            passInput: getInput,
+          ),
           Row(
             children: [
               Expanded(
