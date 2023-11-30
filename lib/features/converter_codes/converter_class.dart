309f5adfbbbc22261844c75778edc830c0d50aed
LETrabado <luisemil.trabado@wvsu.edu.ph>
2023-11-30 08:20:44 +0800

    added measurement units

--- lib/features/converter_codes/converter_class.dart
+++ lib/features/converter_codes/converter_class.dart
@@ -1,6 +1,23 @@
 import 'package:converter/converter.dart';
 
 class ConvertFunct {
+  final List<String> length_unit = [
+    'km',
+    'hm',
+    'm',
+    'dm',
+    'cm',
+    'mm',
+    'in',
+    'ft',
+    'yd',
+    'mi'
+  ];
+  final List<String> Speed_unit = ['m/s', 'km/h', 'ft/s', 'mi/h', 'kn'];
+  final List<String> Temperature_unit = ['K', 'C', 'F', 'R'];
+  final List<String> Mass_unit = ['kg', 'hg', 'g', 'mg', 'oz', 'lb', 't'];
+  final List<String> Time_unit = ['s', 'ms', 'min', 'h', 'd', 'wk', 'mo', 'y'];
+
   num conLength(num input, String in_unit, out_unit) {
     Length initial = Length(input, in_unit);
     num result = initial.valueIn(out_unit);
